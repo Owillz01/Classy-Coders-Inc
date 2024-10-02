35cd801f73593762d049fa463cb61154efb13d92
Owillz01 <omonkhodion@gmail.com>
2024-10-02 13:06:10 +0100

    done with the first two tasks

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,8 +1,20 @@
-class Manager {
+const {Employee} = require('./Employee.js')
+class Manager extends Employee {
+  #employeesManaged;
 
+  constructor(name, position, salary, department, employeesManaged) {
+    super(name, position, salary);
+    this.department = department;
+    this.#employeesManaged = [];
+  }
 
+  getEmployeesManaged() {
+    return this.#employeesManaged;
+  }
 
-    
+  setEmployeesManaged(employee) {
+    this.#employeesManaged.push(employee);
+  }
 }
 
 module.exports = {
